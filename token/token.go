c9a92a0b2474fb0b2ca2f4fe758dd6af92d406c4
edihff <potepoteto910@gmail.com>
2025-05-17 20:55:16 +0900

    tokenのtypeを追加

--- token/token.go
+++ token/token.go
@@ -11,8 +11,15 @@ const (
 	INT   = "INT"
 
 	// 演算子
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// デリミタ
 	COMMA     = ","
